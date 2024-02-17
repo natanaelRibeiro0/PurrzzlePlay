62696d4d726a4c12dfb6dfdf3bf24d769b53aa29
Natanael Ribeiro <natanaelworkstation@gmail.com>
2024-02-16 22:58:03 -0300

    Upload project directory

---
+++ README.md
@@ -0,0 +1,59 @@
+## Topics
+  
+1. [ABOUT THE PROJECT](#about-the-project) 
+2. [DOCUMENTATION](#documentation) 
+3. [SEARCHS AND COPYRIGHT](#searchs-and-copyright)
+
+##  ABOUT THE PROJECT 😽🧩
+
+Puzzleplay is a simple game made with HTML, CSS and JS, without any framework. The objective of the game is to complete the puzzle (in the square) with the pieces in the correct order.
+The game is friend responsiveness to diferent sizes (*812 px, 1024px and 1920 px*)
+
+With the progress in my studies, I started to make projets that chalenge me, that make me go and learn about a new function, event, our method, to train my web programing skills. Because of that, I decided to work with the specific **drag** eventListeners, to understand how the navigator can recognize itens that are being draged, to undertand more about eventListeners, and, off course, resolve a small puzzle (I really love puzzles :P)
+
+The ideia of that litle game came to me when I was playing with a 1000 pieces puzzle, with one make me thing like a explosion on my mind
+
+	" And if make a web puzzle? # 💥# 💥# 💥"	
+
+>[!TIP]
+>The program have cool features, like:
+>1.  Increase the resolution image with double click
+>2. Change the position of the piece if droped in the piece area
+>3. A simple message that appears when the puzzle is complete
+
+##  DOCUMENTATION
+
+> [!note] 
+The HTML and CSS are really simple in this project, but, the `data-position` in HTML is **extremely** important for the page, since the JS in the frontend will be used to fit the pieces together
+
+### FUNCTIONS
+
+> ***dependence( )*** --> That event's is extremely important, because the event need to alternate from the default event
+> 
+> ***createPieces( )*** --> Function that create the pieces and place then
+> 
+> ***fit pieces*** --> That function will check and, if true, will add the piece and handle with remove of classes, but, if not, will remove "is-dragging" class from the piece. 
+> The way used to check if the piece have been dropped in the right place is simple, the program will acess the elemet where the piece have been droped (with the event), and check the `data-position `number, if is equals the piece `data-position` it will fit in
+> 
+> ***push pieces*** --> That function is called when the piece is pushed inside the area where pieces are randomly positioned. 
+> That function will check the position where the mouse (our cursor, whatever) was when the piece was droped, then, will change the position in line, adjusting the height and width
+> 
+>***incress reference image*** --> Used to visualise the resolution image more clearly. 
+>That function basically will add the class `displayAnimation` to the image grown. To go back to the normal size, double click in the image to add  the class `recallAnimation`, that will re-transform the image size
+>
+>***generateNumber*** --> Generate a random number based in two number in the param. Is used to generate a random position to the pieces 
+>
+>***puzzleResolveVerify*** --> Event that change the array "puzzleState" and, if all true, return a alert and finish the game
+>That function will use the `data-position` to know the index that have to change the value to true
+>
+>***placePieces*** --> Will check the size of the screen and put the pieces in the right area 
+>That functions is extremely important because of the responsivity of the page, with that, pieces will be generated in the right place
+>
+>***dependence*** --> That event's is extremely important. Some HTML tags need to have their events changed from default to the drag event works
+
+## SEARCHS AND COPYRIGHT
+
+The [cat image is from](https://www.freepik.com/free-photo/fluffy-kitten-sitting-grass-staring-sunset-playful-generated-by-artificial-intelligence_86175654.htm#query=beautiful%20cat&position=19&from_view=keyword&track=ais&uuid=0fe78b9b-b69e-426b-9785-7411cc3b28ea) and I used the [IMGonline.com.ua](https://www.imgonline.com.ua/eng/cut-photo-into-pieces.php) to cut the images in 15 pieces
+
+### COPYRIGHT ©Natanael Ribeiro
+
---
+++ dist/src/index.js
@@ -0,0 +1,153 @@
+// when all these elements have turned true, a alert will be show in the browser page
+const puzzleState = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false] // --> Variavel que auxilia no reconhecimento de final do puzzle
+
+const puzzleArea = document.querySelector(".puzzle-area"); // --> square that fit in the pieces
+const piecesArea = document.querySelector("aside") // --> where the pieces start
+const referenceImage = document.querySelector(".resolution") // --> image reference off the resolution of the puzzle
+
+// function that create the pieces
+// ***createPieces***
+function createPieces() {
+  for (let index = 0; index < 15; index++) {
+    
+    // make a new piece
+    const piece = document.createElement("img")
+    piece.src = `../../img/puzzle-image/puzzle-image ${index + 1}.png`
+    piece.alt = "piece-image"
+    piece.className = `piece` 
+    piece.draggable = "true"
+    piece.dataset.position = index 
+
+    // put the piece in a area (based in the size of the window)
+    placePieces(innerWidth, piece)
+
+    // add the piece event to drag
+    piece.addEventListener("dragstart", (event) => {
+      event.target.classList.add("is-dragging") 
+    })
+  }
+}
+
+  // event that handle with a try to fit a piece
+  //***fit pieces***
+  puzzleArea.addEventListener("drop", async (event) => {
+    event.preventDefault(event)
+
+    const element = event.target // Event
+    const pieceBeingDraged = document.querySelector(".is-dragging")// Piece that was used to fit
+  
+      // if true: will add the piece and handle with remove of classes
+      // if false: will remove "is-dragging" class from the piece
+
+    if (pieceBeingDraged.dataset.position === element.dataset.position) {
+      event.target.appendChild(pieceBeingDraged) 
+  
+      pieceBeingDraged.style.top = "0px"  
+      pieceBeingDraged.style.right = "0px"
+  
+      pieceBeingDraged.classList.remove("is-dragging") 
+      pieceBeingDraged.classList.remove("piece")       
+      pieceBeingDraged.classList.add("piece-positioned") 
+      
+      pieceBeingDraged.draggable = false
+
+      // will check if the puzzle have been resolved
+      puzzleResolveVerify(pieceBeingDraged.dataset.position)
+    }
+    pieceBeingDraged.classList.remove("is-dragging")   
+  })
+
+  // event that handle with the drop of the piece inside the piece square.
+    // ***push pieces***
+    piecesArea.addEventListener("drop", (event) =>{
+    event.preventDefault()
+    const pieceBeingDraged = document.querySelector(".is-dragging")
+
+    const height = event.clientY // height where the piece was dropped
+    const width = event.clientX //  width where the piece was dropped
+    
+    pieceBeingDraged.style.top = `${height}px` // will change the position of the piece
+    pieceBeingDraged.style.left = `${width}px` // will change the position of the piece
+    
+    pieceBeingDraged.classList.remove("is-dragging")
+  })
+
+// event that will handle with the double click in the resolution image
+// ***incress reference image*** 
+referenceImage.addEventListener("dblclick", () => {  
+  if(referenceImage.classList.contains("displayAnimation")){
+    referenceImage.classList.remove("displayAnimation")
+    referenceImage.classList.add("recallAnimation")
+  } else {
+    referenceImage.classList.remove("recallAnimation")
+    referenceImage.classList.add("displayAnimation")
+  }
+})
+
+// generate a random number based in two number in the param (used to generate a random position to the pieces)
+// ***generateNumber***
+function generateNumber(a, b) {
+  const number = Math.floor(Math.random() * (b - a + 1)) + a
+  return (`${number}px`)
+}
+
+// event that change the array "puzzleState" and, if all true, return a alert and finish the game
+// ***puzzleResolveVerify***
+function puzzleResolveVerify(positionData){
+  puzzleState[positionData] = true
+  if((puzzleState.includes(false))){
+    alert("Puzzle resoleve. Congratulations!! :D")
+    alert("Restart the page to play the game")
+  }
+}
+
+// will check the size of the screen and put the pieces in the right area (extremely important because of the responsivity)
+// ***placePieces***
+function placePieces(pageWidht, piece){
+  //812px (Tablet) position
+  if(pageWidht >= 812 && pageWidht < 1024){
+    piece.style.top = generateNumber(700, 790) 
+    piece.style.right = generateNumber(50, 690) 
+    // Adiciona as imagems no seu espaço
+    piecesArea.append(piece)
+  }
+
+  //1037px (Laptop) position
+  if(pageWidht >= 1024 && pageWidht < 1920){
+    piece.style.top = generateNumber(690, 750) 
+    piece.style.right = generateNumber(50, 850) 
+    // Adiciona as imagems no seu espaço
+    piecesArea.append(piece)
+  }
+
+  //1920px (Monitor)position
+  if(pageWidht >= 1920){
+    piece.style.top = generateNumber(720, 80) 
+    piece.style.right = generateNumber(350, 50) 
+    // Adiciona as imagems no seu espaço
+    piecesArea.append(piece)
+  }
+}
+
+// that event's is extremely important, because the event need to alternate from the default event
+// ***dependence***
+function dependence() {
+  puzzleArea.addEventListener("dragenter", (event) => {
+      event.preventDefault()
+    })
+  
+    puzzleArea.addEventListener("dragover", (event) => {
+      event.preventDefault()
+    })
+  
+    piecesArea.addEventListener("dragenter", (event) => {
+      event.preventDefault()
+    })
+    piecesArea.addEventListener("dragover", (event) => {
+      event.preventDefault()
+    })
+}
+
+// will call functions
+dependence()
+createPieces()
